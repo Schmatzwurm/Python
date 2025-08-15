809a8bc62ad2d91d1b9dea85027e01a5bb29f974
Benjamin Reich <benjamin.reich@gmx.net>
2025-08-15 17:56:08 +0200

    skeleton stuff

--- tank_battle/base/__init__.py
+++ tank_battle/base/__init__.py
@@ -1,6 +1,1 @@
-import tank
-import menu
-import control
 
-import tank_battle.control.hand_controllers as hand_controllers
-import utils
--- tank_battle/game.py
+++ tank_battle/game.py
@@ -1,2 +1,16 @@
 import base
+import time
 
+def loop():
+    while True:
+        time.sleep(0.01)
+
+def main():
+    try:
+        loop()
+    except:
+        pass
+    pass
+
+if __name__ == "__main__":
+    main()
