4dcde04b877030c6e9f7c3b58c675ee725df35cc
Timon Reich <timon.reich@gmx.de>
2025-08-16 14:06:44 +0200

    tank move

--- tank_battle/game.py
+++ tank_battle/game.py
@@ -2,6 +2,7 @@ import modules
 
 from modules.objects import background
 from modules.objects import tank
+from modules.menu import main_menu
 
 import pygame
 import traceback
@@ -9,6 +10,7 @@ import traceback
 from pygame import display
 
 # Must come from options later
+# Also calculate all positions based on screen dimension later
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
@@ -22,13 +24,25 @@ class Game:
         back = background.Background(self._screen)
         back.draw()
 
-        t1 = tank.Tank(self._screen)
-        t1.draw(0, 0)
+        menu = main_menu.Menu(self._screen)
+
+        t1 = tank.Tank(self._screen, init_pos=(0,400), max_pos=(1100, 400), reverse=False)
+        t1.draw()
+
+        t2 = tank.Tank(self._screen, init_pos=(1100, 400), max_pos=(1100, 400), reverse=True)
+        t2.draw()
 
         while not abort:
+            back.draw()
+
             for e in pygame.event.get():
                 if e.type is pygame.QUIT:
                     abort = True
+
+            t1.move(5, 0)
+            t1.draw()
+            t2.move(5, 0)
+            t2.draw()
             pygame.display.update()
             self._clock.tick(100)
 
--- tank_battle/modules/objects/tank.py
+++ tank_battle/modules/objects/tank.py
@@ -3,17 +3,47 @@ import pygame
 from ..base import utils
 
 class Tank:
-    def __init__(self, screen):
+    def __init__(self, screen, size=(180, 150), 
+                 init_pos=(0,0), min_pos=(0,0), max_pos=(100,100), 
+                 reverse=False):
         image_file_path = utils.get_res_file_path('tank.png')
         image = pygame.image.load(image_file_path)
-        self._image_right = pygame.transform.scale(image, (180, 150))
+        self._image_right = pygame.transform.scale(image, size)
         self._image_left = pygame.transform.flip(self._image_right, True, False)
-        self._image = self._image_right
+        if reverse:
+            self._image = self._image_left
+        else:
+            self._image = self._image_right
         self._screen = screen
-        
+        self._reverse = reverse
+        self._pos_x = init_pos[0]
+        self._pos_y = init_pos[1]
+        self._min_pos_x = min_pos[0]
+        self._min_pos_y = min_pos[1]
+        self._max_pos_x = max_pos[0]
+        self._max_pos_y = max_pos[1]
 
-    def draw(self, x, y):
-        self._screen.blit(self._image, (x, y))
+    def draw(self):
+        self._screen.blit(self._image, (self._pos_x, self._pos_y))
 
+    def move(self, delta_x=0, delta_y=0):
+        if self._reverse:
+            delta_x = -delta_x
+            delta_y = -delta_y
 
+        new_pos_x = self._pos_x + delta_x
+        new_pos_y = self._pos_y + delta_y
+
+        if new_pos_x < self._min_pos_x:
+            new_pos_x = self._min_pos_x
+        if new_pos_y < self._min_pos_y:
+            new_pos_y = self._min_pos_y
+
+        if new_pos_x > self._max_pos_x:
+            new_pos_x = self._max_pos_x
+        if new_pos_y > self._max_pos_y:
+            new_pos_y = self._max_pos_y
+
+        self._pos_x = new_pos_x
+        self._pos_y = new_pos_y
     
