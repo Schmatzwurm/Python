37b748b129c02e69d91e25422290859ef1a56642
Timon Reich <timon.reich@gmx.de>
2025-08-25 08:54:58 +0200

    support inversion

--- tank_battle/modules/physics/throwing_path.py
+++ tank_battle/modules/physics/throwing_path.py
@@ -3,46 +3,50 @@ import math
 
 class ThrowingPath:
 
-    def __init__(self, min_pos=(0,0), max_pos=(1000, 1000)):
-        self.angle = 0
-        self.start_time = None
-        self.min_pos = min_pos
-        self.max_pos = max_pos
-        self.start_pos = [0, 0]
-        self.velocity = [0, 0]
-        self.gravity = 9.81  # Gravity constant
+    def __init__(self, min_pos=(0,0), max_pos=(100, 100), invert_x=False, invert_y=False):
+        self._invert_x = invert_x
+        self._invert_y = invert_y
+        self._angle = 0
+        self._start_time = None
+        self._min_pos = min_pos
+        self._max_pos = max_pos
+        self._start_pos = [0, 0]
+        self._velocity = [0, 0]
+        self._gravity = 9.81  # Gravity constant
 
 
     def throw(self, velocity, angle, start_pos):
-        self.start_time = time.time()
-        self.angle = angle
-        self.start_pos = start_pos
-        self.velocity[0] = velocity * math.cos(self.angle)
-        self.velocity[1] = velocity * math.sin(self.angle)
+        self._start_time = time.time()
+        self._angle = angle
+        self._start_pos = start_pos
+        self._velocity[0] = velocity * math.cos(self._angle)
+        self._velocity[1] = velocity * math.sin(self._angle)
 
 
     def update_and_get(self):
-        if self.start_time is None:
-            return self.start_pos
+        if self._start_time is None:
+            return self._start_pos
         current_time = time.time()
-        elapsed_time = current_time - self.start_time
+        elapsed_time = current_time - self._start_time
         return self.get_pos(elapsed_time)
     
 
     def get_pos(self, elapsed_time):
-        if self.velocity is None:
-            return self.start_pos
+        if self._velocity is None:
+            return self._start_pos
 
         return (self.get_x_pos(elapsed_time), self.get_y_pos(elapsed_time))
 
     
     def get_x_pos(self, elapsed_time):
-        pos = self.velocity[0] * elapsed_time + self.start_pos[0]
+        factor = -1 if self._invert_x else 1
+        pos = (factor * self._velocity[0] * elapsed_time) + self._start_pos[0]
 
-        return max(self.min_pos[0], min(self.max_pos[0], pos))
+        return max(self._min_pos[0], min(self._max_pos[0], pos))
 
 
     def get_y_pos(self, elapsed_time):
-        pos = -0.5 * self.gravity * math.pow(elapsed_time, 2) + self.velocity[1] * elapsed_time + self.start_pos[1]
+        factor = -1 if self._invert_y else 1
+        pos = (factor * (-0.5 * self._gravity * math.pow(elapsed_time, 2) + self._velocity[1] * elapsed_time)) + self._start_pos[1]
     
-        return max(self.min_pos[1], min(self.max_pos[1], pos))
+        return max(self._min_pos[1], min(self._max_pos[1], pos))
